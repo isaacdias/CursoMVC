3c201fbcad7440d6b8e190107acd5243787db232
isaacdias <isaacardias@gmail.com>
2020-11-30 20:16:48 -0300

    criado o primeiro migration

--- CursoMVC/Models/Context.cs
+++ CursoMVC/Models/Context.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CursoMVC.Models
 {
-    public class Context
+    public class Context : DbContext
     {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
+        }
     }
 }
