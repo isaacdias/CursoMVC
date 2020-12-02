7bd1c4554d89931b470ec3f2e5afe7c503164008
isaacdias <isaacardias@gmail.com>
2020-12-01 21:05:44 -0300

    criando um app web ASP .NET MVC

--- CursoMVC/Models/Categoria.cs
+++ CursoMVC/Models/Categoria.cs
@@ -13,7 +13,5 @@ namespace CursoMVC.Models
         [Required(ErrorMessage = "O campo descrição é obrigatório.")]
 
         public string Descricao { get; set; }
-
-        public List<Produto> Produtos { get; set; }
     }
 }
